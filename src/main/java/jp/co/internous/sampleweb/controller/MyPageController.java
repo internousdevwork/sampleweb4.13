e44f34a33ca7b3e5f8bfd4701ac41d106531bb17
k-kawamura-internous <k.kawamura@internous.co.jp>
2022-01-19 20:06:41 +0900

    templateブランチ作成

--- src/main/java/jp/co/internous/sampleweb/controller/MyPageController.java
+++ src/main/java/jp/co/internous/sampleweb/controller/MyPageController.java
@@ -1,39 +1,15 @@
 package jp.co.internous.sampleweb.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import jp.co.internous.sampleweb.model.domain.MstUser;
-import jp.co.internous.sampleweb.model.mapper.MstUserMapper;
-import jp.co.internous.sampleweb.model.session.LoginSession;
-
-/**
- * マイページに関する処理を行うコントローラー
- * @author インターノウス
- *
- */
 @Controller
 @RequestMapping("/sampleweb/mypage")
 public class MyPageController {
 	
-	@Autowired
-	private MstUserMapper userMapper;
-	
-	@Autowired
-	private LoginSession loginSession;
-	
-	/**
-	 * マイページ画面を初期表示する。
-	 * @param m 画面表示用オブジェクト
-	 * @return マイページ画面
-	 */
 	@RequestMapping("/")
-	public String index(Model m) {
-		MstUser user = userMapper.findByUserNameAndPassword(loginSession.getUserName(), loginSession.getPassword());
-		m.addAttribute("user", user);
-		m.addAttribute("loginSession", loginSession);
+	public String index() {
 		return "my_page";
 	}
+
 }
