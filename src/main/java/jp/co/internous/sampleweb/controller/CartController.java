5e99ae9577d59895a11a8c74bbdc07cba623bb89
k-kawamura-internous <k.kawamura@internous.co.jp>
2022-03-11 14:34:28 +0900

    javadocを削除

--- src/main/java/jp/co/internous/sampleweb/controller/CartController.java
+++ src/main/java/jp/co/internous/sampleweb/controller/CartController.java
@@ -3,11 +3,7 @@ package jp.co.internous.sampleweb.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- * 
- * @author インターノウス
- *
- */
+
 @Controller
 @RequestMapping("/sampleweb/cart")
 public class CartController {
